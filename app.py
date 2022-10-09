68227a1beb5b5e79cff011f23cbd265ba408844f
rivo2302 <rivo2302@gmail.com>
2022-10-09 21:38:25 +0300

    [FEAT] RPC Odoo xmll rpc

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from os import environ as env
 from config import CONFIG as CONF
+from utils.rpc import RPC
 
 app = Flask(__name__)
+
+odoo = {
+    "HOST": env.get("ODOO_HOST"),
+    "PORT": env.get("ODOO_PORT"),
+    "DB": env.get("ODOO_DB"),
+    "USER": env.get("ODOO_USER"),
+    "PASSWORD": env.get("ODOO_PASSWORD"),
+}
+rpc = RPC(odoo)
+
+x = rpc.execute("res.partner", "search_read", [], ["name"])
+print(x)
 
 if __name__ == "__main__":
     app.run(CONF["HOST"], CONF["PORT"], CONF["DEBUG"])
--- utils/rpc.py
+++ utils/rpc.py
@@ -1,14 +1,32 @@
-import odoorpc
+import xmlrpc.client
 from os import environ as env
 
-CONF = {
-    "HOST": env.get("DB_HOST"),
-    "PORT": env.get("DB_PORT"),
-    "DB": env.get("DB_NAME"),
-    "USER": env.get("DB_USER"),
-    "PASSWD": env.get("DB_PASSWD"),
-}
 
-odoo = odoorpc.ODOO(CONF["HOST"], port=CONF["PORT"])
-print(odoo.db.list())
-odoo.login(CONF["DB"], CONF["USER"], CONF["PASSWD"])
+class RPC:
+    def __init__(self, conf) -> None:
+        self.conf = conf
+        pass
+
+    def common(self):
+        return xmlrpc.client.ServerProxy(
+            f"{self.conf['HOST']}/xmlrpc/2/common", allow_none=True
+        )
+
+    def login(self):
+        return self.common().authenticate(
+            self.conf["DB"], self.conf["USER"], self.conf["PASSWORD"], {}
+        )
+
+    def execute(self, model, method, *args):
+        if not self.login():
+            return False
+        return xmlrpc.client.ServerProxy(
+            f"{self.conf['HOST']}/xmlrpc/2/object", allow_none=True
+        ).execute_kw(
+            self.conf["DB"],
+            self.login(),
+            self.conf["PASSWORD"],
+            model,
+            method,
+            args,
+        )
