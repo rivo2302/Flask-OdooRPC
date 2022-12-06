b37e1cbb422733e3360dca82892be849b3df2a19
rivo2302 <rivo2302@gmail.com>
2022-12-06 22:57:50 +0300

    [ADD] Token required decorators

---
+++ api/ressources/auth.py
@@ -0,0 +1,21 @@
+from flask import request, jsonify, make_response
+from os import environ as env
+from functools import wraps
+
+
+# Authentication decorator
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if "access-token" in request.headers:
+            token = request.headers["access-token"]
+        if not token:  # throw error if no token provided
+            return make_response(
+                jsonify({"message": "A valid token is missing!"}), 401
+            )
+        if token == env.get("TOKEN"):
+            return f(*args, **kwargs)
+        return make_response(jsonify({"message": "invalid token"}), 401)
+
+    return decorator
